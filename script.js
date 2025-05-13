f8a677bf807eb63cbf0dbb74ba62432cc4d4ab85
alufena <andersonpolv@gmail.com>
2025-05-13 17:24:14 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -15,14 +15,16 @@ let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = +document.querySelector('.guess').value;
+    const guess = +(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-    if (!guess) {
+    if (!guess) { // input vazio ou com 0
         document.querySelector('.message').textContent = 'No number';
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct number';
+        document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').style.width = '30rem' // atribua-se a uma string sempre que manipular um inline style
     } else if (guess > secretNumber) {
-        if (score > 0) {
+        if (score > 1) {
             document.querySelector('.message').textContent = 'Too high';
             score--; // score = score - 1;
             document.querySelector('.score').textContent = score;
@@ -31,9 +33,9 @@ document.querySelector('.check').addEventListener('click', function () {
             document.querySelector('.score').textContent = 0;
         }
     } else if (guess < secretNumber) {
-        if (score > 0) {
+        if (score > 1) {
             document.querySelector('.message').textContent = 'Too low';
-            score--; // score = score - 1;
+            score--;
             document.querySelector('.score').textContent = score;
         } else {
             document.querySelector('.message').textContent = 'Game over';
