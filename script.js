38e2cf61af2893d0f6f42d0e935c7076d5ffe435
alufena <andersonpolv@gmail.com>
2025-05-13 16:18:09 -0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-'use strict'
+'use strict';
+
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 13; // não se pode atribuir um elemento inteiro sozinho, porque tal elemento possui muito mais que só o content. por isso o ".textContent" (sem ele dá erro)
+document.querySelector('.score').textContent = 10;
+console.log(document.querySelector('.guess').value); // retorna "<empty string>" por enquanto
+document.querySelector('.guess').value = 2; // põe um valor padrão
