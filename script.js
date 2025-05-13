77d337a2b69a26b1f7c3255660479c5ee7f6de6b
alufena <andersonpolv@gmail.com>
2025-05-13 20:03:34 -0300

    dry

--- script.js
+++ script.js
@@ -12,48 +12,71 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+};
+
 // document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = +document.querySelector('.guess').value;
+    const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     if (!guess) {
         // input vazio ou com 0
-        document.querySelector('.message').textContent = 'No number';
-    } else if (guess === secretNumber) { // acerto
-        document.querySelector('.message').textContent = 'Correct number';
+        // document.querySelector('.message').textContent = 'No number';
+        displayMessage('No number!');
+    } else if (guess === secretNumber) {
+        // acerto
+        // document.querySelector('.message').textContent = 'Correct number';
+        displayMessage('Correct number!');
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem'; // atribua-se a uma string sempre que manipular um inline style
         if (score > highScore) {
             highScore = score;
-            document.querySelector('.highscore').textContent = score;
+            document.querySelector('.highscore').textContent = highScore;
         }
-    } else if (guess > secretNumber) {
+    } else {
+        // } else if (guess !== secretNumber) {
+        // refactor
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too high';
-            score--; // score = score - 1;
-            document.querySelector('.score').textContent = highScore;
-        } else {
-            document.querySelector('.message').textContent = 'Game over';
-            document.querySelector('.score').textContent = 0;
-        }
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too low';
+            // document.querySelector('.message').textContent =
+            displayMessage(guess > secretNumber ? 'Too high' : 'Too low!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = 'Game over';
+            // document.querySelector('.message').textContent = 'Game over';
+            displayMessage('Game over!');
             document.querySelector('.score').textContent = 0;
         }
     }
+    // } else if (guess > secretNumber) {
+    //     if (score > 1) {
+    //         document.querySelector('.message').textContent = 'Too high';
+    //         score--; // score = score - 1;
+    //         document.querySelector('.score').textContent = highScore;
+    //     } else {
+    //         document.querySelector('.message').textContent = 'Game over';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    // } else if (guess < secretNumber) {
+    //     if (score > 1) {
+    //         document.querySelector('.message').textContent = 'Too low';
+    //         score--;
+    //         document.querySelector('.score').textContent = score;
+    //     } else {
+    //         document.querySelector('.message').textContent = 'Game over';
+    //         document.querySelector('.score').textContent = 0;
+    //     }
+    //  }
 });
 
-document.querySelector('.again').addEventListener('click', function () { // restaura as condições iniciais
+document.querySelector('.again').addEventListener('click', function () {
+    // restaura as condições iniciais
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.message').textContent = 'Start guessing...'
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     document.querySelector('.score').textContent = score; // reseta score
     document.querySelector('.guess').value = ''; // limpa input
     document.querySelector('body').style.backgroundColor = '#222';
